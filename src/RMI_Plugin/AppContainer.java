5f280f93188b57a132e0845548a0ebaf035f24e7
Przemyslaw <przemyslaw.skoczewski@gmail.com>
2020-04-03 16:49:06 +0200

    Final version

--- src/RMI_Plugin/AppContainer.java
+++
@@ -1,8 +0,0 @@
-package RMI_Plugin;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface AppContainer extends Remote {
-    Object run(Plugin p) throws RemoteException;
-}
--- src/RMI_Plugin/Plugin.java
+++
@@ -1,6 +0,0 @@
-package RMI_Plugin;
-
-public interface Plugin extends java.io.Serializable
-{
-    Object compute();
-}
